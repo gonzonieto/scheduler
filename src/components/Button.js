47d728d105b59bb11826fd4bcec4f9f9d4c233d4
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-19 19:44:42 -0400

    first commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import classNames from "classnames";
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
 export default function Button(props) {
-   return <></>;
+  const { confirm, danger, disabled, onClick } = props;
+
+  const buttonClass = classNames(
+    'button',
+    {'button--confirm': confirm},
+    {'button--danger': danger}
+  );
+
+  return (
+    <button className={buttonClass} onClick={onClick} disabled={disabled}>
+      {props.children}
+    </button>
+  );
 }
---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+function DayList(props){
+  const { days, day, setDay } = props;
+
+  const dayList = days.map((item) => (
+    <DayListItem
+      key={item.id}
+      name={item.name}
+      spots={item.spots}
+      selected={item.name === day}
+      setDay={setDay}
+    />
+  ));
+  
+  return(
+    <ul>
+      {dayList}
+    </ul>
+  );
+};
+export default DayList

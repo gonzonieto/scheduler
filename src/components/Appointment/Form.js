e963651f6faf7371604027c2b3d4793d0b3a10c1
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 19:28:51 -0400

    set up state control and passing props to InterviewerList

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,9 +3,10 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props){
-  const { interviewer, interviewers, onSave, onCancel } = props;
-  
-  const [student, setStudent] = useState(props.student);
+  const { interviewers, onSave, onCancel } = props;
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   return(
     <main className="appointment__card appointment__card--create">
@@ -20,7 +21,7 @@ export default function Form(props){
             onChange={(e) => setStudent(e.target.value)}
           />
         </form>
-        <InterviewerList interviewers={interviewers} value={interviewer} />
+        <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">

997ab0ef05251b9edda7a98f7b66dde40732e615
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 19:16:52 -0400

    made Edit form editable

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-export default function Form({ student, interviewer, interviewers, onSave, onCancel }){
+export default function Form(props){
+  const { interviewer, interviewers, onSave, onCancel } = props;
   
+  const [student, setStudent] = useState(props.student);
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -14,6 +17,7 @@ export default function Form({ student, interviewer, interviewers, onSave, onCan
             type="text"
             placeholder="Enter Student Name"
             value={student}
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
         <InterviewerList interviewers={interviewers} value={interviewer} />

a3e6a3f3f8c4d9c7074860e40f8e0c4042dea91c
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 18:23:28 -0400

    replace hard-coded values with props

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Button from "components/Button";
 
-export default function Confirm(){
+export default function Confirm({ message, onConfirm, onCancel }){
+  // props - message:String, onConfirm:Function, onCancel:Function
+
   return(
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold"></h1>
+      <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
-        <Button danger></Button>
-        <Button danger></Button>
+        <Button danger onClick={onCancel} >Cancel</Button>
+        <Button danger onClick={onConfirm} >Confirm</Button>
       </section>
     </main>
   );

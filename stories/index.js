a3fa021d9056b327c32a16c8a0a921de2d9df4c6
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 18:13:43 -0400

    add stories for Empty and Show components

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import InterviewerList from "components/InterviewerList";
 import InterviewerListItem from "components/InterviewerListItem";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
 .addParameters({
@@ -149,5 +151,7 @@ storiesOf("Appointment", module)
     ]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Time 12pm", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />);

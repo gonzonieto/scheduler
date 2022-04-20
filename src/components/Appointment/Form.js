e9362147ce50ca49b76b05b0c22c4aa7fc3738c2
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 19:10:37 -0400

    create Form component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,29 @@
+import React from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form({ student, interviewer, interviewers, onSave, onCancel }){
+  
+  return(
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+          />
+        </form>
+        <InterviewerList interviewers={interviewers} value={interviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={onCancel} >Cancel</Button>
+          <Button confirm onClick={onSave} >Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+};

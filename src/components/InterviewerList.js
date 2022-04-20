17121e638423fa723517063b705792bc1e2275c3
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 02:05:50 -0400

    use map() to create array of components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,25 @@
 import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
 
 import "components/InterviewerList.scss"
 
 function InterviewerList(props){
   const { interviewers, setInterviewer, interviewer} = props;
-  
+
+  const interviewerList = interviewers.map((item) => (
+    <InterviewerListItem
+      key={item.id}
+      name={item.name}
+      avatar={item.avatar}
+      selected={item.id === interviewer}
+      setInterviewer={() => setInterviewer(item.id)}
+    />
+  ));
+
   return(
     <section className="interviewers">
       <h4 className="interviewers__header text--light"></h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );
 };

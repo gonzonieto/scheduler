5b5f0a10e8409a6d81e7db06436600cbe5effda6
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 17:00:03 -0400

    create basic Appointment component with string formatter

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "components/Appointment/styles.scss";
 
 export default function Appointment(props){
+  const formatAppointmentString = (time) => {
+    return time ? `Appointment at ${time}` : "No Appointments";
+  };
+
   return(
-    <article className="appointment"></article>
+    <article className="appointment">{formatAppointmentString(props.time)}</article>
   );
 };

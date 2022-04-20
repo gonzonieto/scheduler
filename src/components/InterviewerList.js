9f123a16be69ba3ba85144fb00cf03c43cf88a5b
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 19:29:36 -0400

    add missing text for interviewers header

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -18,7 +18,7 @@ export default function InterviewerList(props){
 
   return(
     <section className="interviewers">
-      <h4 className="interviewers__header text--light"></h4>
+      <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );

f852704bba78128853274db474e079a7cc8b8eec
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-20 20:46:22 -0400

    rendering Header and conditional render of Show/Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 import "components/Appointment/styles.scss";
 
-export default function Appointment(props){
-  const formatAppointmentString = (time) => {
-    return time ? `Appointment at ${time}` : "No Appointments";
-  };
+export default function Appointment({ time, interview }){
+  // const formatAppointmentString = (time) => {
+  //   return time ? `Appointment at ${time}` : "No Appointments";
+  // };
 
   return(
-    <article className="appointment">{formatAppointmentString(props.time)}</article>
+    <article className="appointment">
+      <Header time={time}>
+      </Header>
+      {interview ? <Show /> : <Empty />}
+    </article>
   );
 };

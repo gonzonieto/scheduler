8b1e59aeb0494a0018a4731ecb44d4cd49381c0d
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-22 13:25:17 -0400

    refactor getAppointmentsForDay()

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,11 @@
 export function getAppointmentsForDay(state, day) {
-  const result = [];
   const selectedDay = state.days.find((item) => item.name === day);
 
-  if (selectedDay) {
-    result.push(...selectedDay.appointments.map((appointmentID) => state.appointments.find((appointment) => appointment.id === appointmentID)));
-  }
-
-  return result;
+  if (!state.days || !selectedDay) return [];
+  
+  return selectedDay.appointments.map(
+    appointmentID => state.appointments.find(appointment => appointment.id === appointmentID)
+  )
 };
 
 // export function getInterview(state, interview) {

5ff6ab8f69d57a1c11d2242e041a8f6a46d7c15d
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-22 17:40:06 -0400

    complete getInterview() and fix bug in getAppointmentsForDay()

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,17 +2,26 @@ export function getAppointmentsForDay(state, day) {
   const selectedDay = state.days.find((item) => item.name === day);
 
   if (!state.days || !selectedDay) return [];
-  
+
   // from the array of appointments for the selected day, 
   return selectedDay.appointments.map(
-    appointmentID => state.appointments.find(
+    appointmentID => Object.values(state.appointments).find(
       appointment => appointment.id === appointmentID
     )
   )
 };
 
-// export function getInterview(state, interview) {
-//   return interview
-//     ? {student: interview.student, interviewer: state.interviewers[interview.interviewer - 1]}
-//     : null;
-// };
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  // interview is an Object with student:String, interviewer:integer
+  // this function returns an Object with student:String, interviewer:Object
+  
+  const result = {};
+  const interviewerID = interview.interviewer;
+  const interviewers = Object.values(state.interviewers);
+  
+  result.student = interview.student;
+  result.interviewer = interviewers.find(interviewer => interviewer.id === interviewerID);
+  
+  return result;
+};

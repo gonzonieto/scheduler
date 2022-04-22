45f79414a213cc00bfb671750da73c5c72c6ad22
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-22 18:51:51 -0400

    add 'default' keyword to function export

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function useVisualMode(initial) {
+export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   return { mode };
 };

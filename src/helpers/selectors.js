695ffa39b2bdc5cdc37d45bca8fdf1d7731dd16f
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-22 13:09:13 -0400

    fix off-by-1 bug when indexing the day's appointments from state

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,8 +3,14 @@ export function getAppointmentsForDay(state, day) {
   const selectedDay = state.days.find((item) => item.name === day);
 
   if (selectedDay) {
-    result.push(...selectedDay.appointments.map((appointment) => state.appointments[appointment]));
+    result.push(...selectedDay.appointments.map((appointmentID) => state.appointments.find((appointment) => appointment.id === appointmentID)));
   }
 
   return result;
-}
+};
+
+// export function getInterview(state, interview) {
+//   return interview
+//     ? {student: interview.student, interviewer: state.interviewers[interview.interviewer - 1]}
+//     : null;
+// };

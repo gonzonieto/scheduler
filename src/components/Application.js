1028354c19962cc3131c62f6243eafbc9c79f680
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-22 15:18:16 -0400

    complete implementation for appointments, and starting to implement interviewers API request

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Button from "components/Button";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -13,26 +14,38 @@ export default function Application(props) {
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = (day) => setState(prev => ({ ...prev, day }));
 
-  const setDays = (days) => setState(prev => ({ ...prev, days }));
-
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then((response) => setDays(response.data))
-      .catch((error) => console.log('LOGGING "error":', error));
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers'),
+    ]).then((all) => {
+      // console.log('DAYS');
+      // console.log(all[0].data);
+      // console.log('APPOINTMENTS');
+      // console.log(Object.values(all[1].data));
+      // console.log('INTERVIEWERS');
+      // console.log(Object.values(all[2].data));
+      setState((prev) => ({ ...prev, days: all[0].data, appointments: Object.values(all[1].data) }));
+    })
+    .catch((error) => console.log('LOGGING "error":', error));
   }, []);
 
   const appointmentList =
-    Object.values(appointments)
+    dailyAppointments
       .map((appointment) => (
         <Appointment
           key={appointment.id}
           {...appointment}
         />
-      )); 
+      ));
 
   return (
     <main className="layout">

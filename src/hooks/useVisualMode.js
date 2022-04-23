9ff92dd1176fd89baa9014274dc2e05fe0d305da
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-22 20:16:40 -0400

    transition() now updates history and back() won't pop the last history element

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,27 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  
+  // history is a stack
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => setMode(newMode);
+  const transition = (newMode) => {
+    const newHistory = [...history];
+    newHistory.push(newMode);
+    
+    setMode(newMode);
+    setHistory(newHistory);
+  };
+  
+  const back = () => {
+    const newHistory = [...history];
 
-  const back = () => undefined;
+    // only pop if we're not at the final element of history
+    if (newHistory.length > 1) newHistory.pop();
+
+    setHistory(newHistory);
+    setMode(newHistory[newHistory.length - 1]);
+  };
   
   return { mode, transition, back };
 };

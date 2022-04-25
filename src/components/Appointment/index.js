d527c0c68cbc0f7d707550c454c7a9f8a73b7482
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-25 16:48:28 -0400

    create save() and pass it to Form so new interviews can be saved

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,40 +1,51 @@
-import React from "react";
-import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Empty";
-import Show from "components/Appointment/Show";
-import Form from "components/Appointment/Form";
-import useVisualMode from "hooks/useVisualMode";
-import "components/Appointment/styles.scss";
+import React from 'react';
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Form from 'components/Appointment/Form';
+import useVisualMode from 'hooks/useVisualMode';
+import 'components/Appointment/styles.scss';
 
-export default function Appointment({ time, interview, interviewers }){
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE";
+export default function Appointment({
+  id,
+  time,
+  interview,
+  interviewers,
+  bookInterview,
+}) {
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
 
-  const { mode, transition, back } = useVisualMode(
-    interview ? SHOW : EMPTY
-  );
-  
-  return(
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    bookInterview(id, interview);
+  };
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+  return (
     <article className="appointment">
-      <Header time={time}>
-      </Header>
+      <Header time={time}></Header>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={interview.student}
           interviewer={interview.interviewer}
-          onEdit={() => console.log("Clicked onEdit")}
-          onDelete={() => console.log("Clicked onDelete")}
+          onEdit={() => console.log('Clicked onEdit')}
+          onDelete={() => console.log('Clicked onDelete')}
         />
       )}
       {mode === CREATE && (
         <Form
           interviewers={interviewers}
-          onSave={() => console.log("Clicked onSave")}
+          onSave={(name, interviewer) => save(name, interviewer)}
           onCancel={() => back()}
         />
       )}
     </article>
   );
-};
+}

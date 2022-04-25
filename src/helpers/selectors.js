991d98888ec57cb45e13372e6bdad60d69faf5c7
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-25 18:00:55 -0400

    refactor now that interviewers is an object

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,48 +1,44 @@
 export function getAppointmentsForDay(state, today) {
   const selectedDay = state.days.find((day) => day.name === today);
-  
+
   if (!state.days || !selectedDay) return [];
 
   const todaysAppointmentIDs = selectedDay.appointments;
   const appointmentsArray = Object.values(state.appointments);
 
   // map the array of appointment IDs into an array of appointment objects
-  return todaysAppointmentIDs.map(id => (
-    appointmentsArray.find(appointment => (
-      appointment.id === id
-    ))
-  ))
-};
+  return todaysAppointmentIDs.map((id) =>
+    appointmentsArray.find((appointment) => appointment.id === id)
+  );
+}
 
 export function getInterviewersForDay(state, today) {
-  const interviewersArray = Object.values(state.interviewers);
+  const interviewers = state.interviewers;
   const selectedDay = state.days.find((day) => day.name === today);
-  
-  if (!interviewersArray.length || !selectedDay) return [];
+
+  if (!Object.keys(interviewers).length || !selectedDay) return [];
 
   const todaysInterviewerIDs = selectedDay.interviewers;
 
   //map the array of interviewer IDs into an array of interviewer objects
-  return todaysInterviewerIDs.map(id => (
-    interviewersArray.find(interviewer => (
-      interviewer.id === id
-    ))
-  ));
-};
+  return todaysInterviewerIDs.map((id) => interviewers[id]);
+}
 
 export function getInterview(state, interview) {
-  // if there is no interview in this timeslot, return null 
+  // if there is no interview in this timeslot, return null
   if (!interview) return null;
 
   // interview is an Object with student:String, interviewer:integer
   // this function returns an Object with student:String, interviewer:Object
-  
+
   const result = {};
   const interviewerID = interview.interviewer;
   const interviewers = Object.values(state.interviewers);
-  
+
   result.student = interview.student;
-  result.interviewer = interviewers.find(interviewer => interviewer.id === interviewerID);
-  
+  result.interviewer = interviewers.find(
+    (interviewer) => interviewer.id === interviewerID
+  );
+
   return result;
-};
+}

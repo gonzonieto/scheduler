4d3dba94a1af98e5812806ff972b7c135967e4b9
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-25 16:47:58 -0400

    add bookInterview() and pass it to Appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,39 +1,47 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import Button from "components/Button";
-import DayList from "components/DayList";
-import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import Button from 'components/Button';
+import DayList from 'components/DayList';
+import Appointment from 'components/Appointment';
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from 'helpers/selectors';
 
-import "components/Application.scss";
+import 'components/Application.scss';
 
 export default function Application() {
-
   const [state, setState] = useState({
-    day: "Monday",
+    day: 'Monday',
     days: [],
     appointments: {},
     interviewers: {},
   });
 
-  const setDay = (day) => setState(prev => ({ ...prev, day }));
-  
   useEffect(() => {
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers'),
-    ]).then((all) => {
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: Object.values(all[1].data),
-        interviewers: Object.values(all[2].data),
-      }));
-    })
-    .catch((error) => console.log('LOGGING "error":', error));
+    ])
+      .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: Object.values(all[1].data),
+          interviewers: Object.values(all[2].data),
+        }));
+      })
+      .catch((error) => console.log('LOGGING "error":', error));
   }, []);
+
+  const bookInterview = (id, interview) => {
+    console.log('LOGGING bookInterview: ', id, interview);
+  };
+
+  const setDay = (day) => setState((prev) => ({ ...prev, day }));
 
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
@@ -47,6 +55,7 @@ export default function Application() {
         interviewers={interviewers}
         {...appointment}
         interview={interview}
+        bookInterview={bookInterview}
       />
     );
   });
@@ -54,27 +63,29 @@ export default function Application() {
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu">
-        <DayList
-          days={state.days || []}
-          value={state.day || ""}
-          onChange={setDay}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
         />
-      </nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days || []}
+            value={state.day || ''}
+            onChange={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        <Button confirm onClick={() => console.log('Button clicked!')}>Bouton</Button>
+        <Button confirm onClick={() => console.log('Button clicked!')}>
+          Bouton
+        </Button>
         {schedule}
         <Appointment key="last" time="5pm" />
       </section>

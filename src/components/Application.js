d79cbc965b9cd294ad8041c12b35797915d7eb19
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-25 22:08:02 -0400

    allow interviews to be deleted

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import Button from 'components/Button';
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment';
@@ -39,7 +38,6 @@ export default function Application() {
 
   const bookInterview = (id, interview) => {
     // This function creates new appointment object with the details of the interview that was booked, makes an axios PUT request to update the appointment in the database, updates state, and returns a Promise for the axios PUT request so the Appointment component that called this function can transition view modes once the database has been updated.
-
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview },
@@ -62,6 +60,24 @@ export default function Application() {
 
   const cancelInterview = (id) => {
     // This function deletes an interview from the database by using the id to find the right appointment slot, and setting its 'interview' property to null
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() => {
+        setState({ ...state, appointments });
+      })
+      .catch((error) => {
+        console.log(`Axios error to PUT /appointments/${id}`, error);
+      });
   };
 
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
@@ -79,6 +95,7 @@ export default function Application() {
         {...appointment}
         interview={interview}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
